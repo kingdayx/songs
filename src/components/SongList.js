07351506f4e43571fbf331560e80b5a4fe4990df
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-04-19 02:10:51 -0400

    gave some more semantic styling and created new component
    gave more semantic styling and created SongDetail component to render the title and duration of each song on button click

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
+import {selectSong} from '../actions/index';
 
 class SongList extends React.Component{
     renderList(){
@@ -8,7 +8,7 @@ class SongList extends React.Component{
             return(
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button className="ui button primary" onClick={()=>this.props.selectSong(song)} >Select</button>
                     </div>
                     <div className="content" >{song.title} </div>
                 </div>
@@ -28,4 +28,4 @@ const mapStateToProps = (state)=>{
     return {songs:state.songs};
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps,{selectSong})(SongList);
